c21205aeb36322190f22d52bb256d6a3782fe36d
Kyleigh Thurman <kyleigh.e.thurman@gmail.com>
2016-07-07 21:17:21 -0500

    committing to github

---
+++ scripts/hi.js
@@ -0,0 +1,14 @@
+// Commands:
+// hi - Hubot will respond with "Hello, from Krit!"
+// hubot bye - Hubot will respond with "Goodbye, from Krit!"
+
+module.exports = function(robot) {
+
+  robot.hear (/hi/i, function (msg) {
+    msg.send("Hello, from Krit!");
+  });
+
+  robot.respond (/bye/i, function (msg) {
+    msg.send("Goodbye, from Krit!");
+  });
+};
